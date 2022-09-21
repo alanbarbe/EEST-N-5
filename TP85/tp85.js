31529adcb5d7383a1274a7ad848827c6b994e67a
alanbarbe <82899662+alanbarbe@users.noreply.github.com>
2022-09-21 18:06:20 -0300

    Add files via upload

---
+++ TP85/tp85.js
@@ -0,0 +1,89 @@
+const http = require('http')
+const fs = require('fs')
+
+const mime = {
+  'html': 'text/html',
+  'css': 'text/css',
+  'jpg': 'image/jpg',
+  'ico': 'image/x-icon',
+  'mp3': 'audio/mpeg3',
+  'mp4': 'video/mp4'
+}
+
+const servidor = http.createServer((pedido, respuesta) => {
+  const url = new URL('http://localhost:8888' + pedido.url)
+  let camino = 'public' + url.pathname
+  if (camino == 'public/')
+    camino = 'public/index.html'
+  encaminar(pedido, respuesta, camino)
+})
+
+servidor.listen(8888)
+
+
+function encaminar(pedido, respuesta, camino) {
+  console.log(camino)
+  switch (camino) {
+    case 'public/recuperardatos': {
+      recuperar(pedido, respuesta)
+      break
+    }
+    default: {
+      fs.stat(camino, error => {
+        if (!error) {
+          fs.readFile(camino, (error, contenido) => {
+            if (error) {
+              respuesta.writeHead(500, { 'Content-Type': 'text/plain' })
+              respuesta.write('Error interno')
+              respuesta.end()
+            } else {
+              const vec = camino.split('.')
+              const extension = vec[vec.length - 1]
+              const mimearchivo = mime[extension]
+              respuesta.writeHead(200, { 'Content-Type': mimearchivo })
+              respuesta.write(contenido)
+              respuesta.end()
+            }
+          })
+        } else {
+          respuesta.writeHead(404, { 'Content-Type': 'text/html' })
+          respuesta.write('<!doctype html><html><head></head><body>Recurso inexistente</body></html>')
+          respuesta.end()
+        }
+      })
+    }
+  }
+}
+
+class CPerson {
+  constructor(nombre, edad) {
+    this.nombre = nombre
+    this.edad = edad
+  }
+  getNombre() { return this.nombre }
+    getEdad() { return this.edad }
+Display(){
+return "Nombre: " + this.nombre + "</br>" + "Edad: " + this.edad;
+}
+}
+
+function recuperar(pedido, respuesta) {
+  let info = ''
+  pedido.on('data', datosparciales => {
+    info += datosparciales
+  })
+  pedido.on('end', () => {
+    const formulario = new URLSearchParams(info)
+    console.log(formulario)
+    respuesta.writeHead(200, { 'Content-Type': 'text/html' })
+    const persona = new CPerson(formulario.get('nombre'), formulario.get('edad'))
+    const pagina =
+      `<!doctype html><html><head></head><body>
+        ${persona.Display()} </br>
+     <a href="index.html">Retornar</a>
+     </body></html>`
+    respuesta.end(pagina)
+  })
+}
+
+console.log('Servidor web iniciado')
